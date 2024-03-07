ae6d995b939e034241d5bf0bacff6d6a319fd103
vkaraman299 <154706592+vkaraman299@users.noreply.github.com>
2024-03-07 20:05:43 +0100

    Update Proje1.java

--- src/our_project/Proje1.java
+++ src/our_project/Proje1.java
@@ -4,6 +4,8 @@ public class Proje1 {
     public static void main(String[] args) {
         System.out.println("----------------------");
         // ssa
+        // github uzerinden degisiklik 
+        
 
     }
 }
